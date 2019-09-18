7fe3912ad102d65e06eb8cf97ffc465d512e9751
Vahan92 <vahan.kalenteryan@gmail.com>
2019-09-18 06:05:24 -0700

    xxx

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 
 import Registration from './components/registration';
 import Login from './components/login';
-
+import Panel from './components/panel';
 
 function App() {
   return (
     <div className="App">
       {/* <Registration/> */}
-      <Login/>
+      {/* <Login/> */}
+      <Panel/>
     </div>
   );
 }
---
+++ front-end/src/components/panel_components/addUser.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function addUser() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default addUser
